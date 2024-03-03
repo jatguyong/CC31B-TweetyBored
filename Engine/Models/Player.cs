34bcb1eb8b0979aeb8346e79af45152e94332625
JENNY ANN GUYONG <jtg6745@students.uc-bcf.edu.ph>
2024-03-03 22:51:12 +0800

    Implement event handler

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -7,14 +8,102 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
+        // backing variables
+        private string _name;
+        private string _characterClass;
+        private int _level;
+        private int _coin;
+        private int _hitPoints;
+        private int _experiencePoints;
+
         // properties
-        public string Name { get; set; }
-        public string CharacterClass { get; set; }
-        public int Level { get; set; }
-        public int Coin { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+                // call this method whenever a property is changed
+            }
+        }
+        public string CharacterClass
+        {
+            get
+            {
+                return _characterClass;
+            }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged("CharacterClass");
+            }
+        }
+        public int Level
+        {
+            get
+            {
+                return _level;
+            }
+            set
+            {
+                _level  = value;
+                OnPropertyChanged("Level");
+            }
+        }
+        public int Coin {
+            get
+            {
+                return _coin;
+            }
+            set
+            {
+                _coin = value;
+                // set the valie of _coin to whatever value is passed
+                OnPropertyChanged("Coin");
+                // this notifies that the Coin value has changed and refreshes the value of Coin in the UI
+            }
+        }
+        public int HitPoints
+        {
+            get
+            {
+                return _hitPoints;
+            }
+            set
+            {
+                _hitPoints  = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+        public int ExperiencePoints
+        {
+            get
+            {
+                return _experiencePoints;
+            }
+            set
+            {
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        // whenever an event occurs, this event is triggered
+
+        public virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            // if this method is called, invoke the OnPropertyChanged event handler
+            // to change the corresponding value of the passed propertyName in the UI
+        }
     }
 }

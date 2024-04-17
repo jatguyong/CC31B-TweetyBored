3e8ff9d1f1c0810a67253070dabdd9502d100552
JENNY ANN GUYONG <jtg6745@students.uc-bcf.edu.ph>
2024-04-17 23:26:40 +0800

    xxaml

--- Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
+++ Engine/obj/Debug/net8.0/Engine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Engine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+893d58d5ea4d32fd190c2e5ffa136a4b4cb2fa46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d43d2762f8c56c4d87f0f8dc183b2c44f12b8fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Engine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Engine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
